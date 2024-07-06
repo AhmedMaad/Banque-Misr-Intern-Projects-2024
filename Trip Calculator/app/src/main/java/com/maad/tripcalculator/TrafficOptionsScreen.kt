faba9aee748d8a0dc5948a0a0060f213344e8206
AhmedMaad <atef.ahmed1341@gmail.com>
2024-07-06 06:46:35 +0200

    Calculating Trip Price

--- Trip Calculator/app/src/main/java/com/maad/tripcalculator/TrafficOptionsScreen.kt
+++ Trip Calculator/app/src/main/java/com/maad/tripcalculator/TrafficOptionsScreen.kt
@@ -1,90 +1,90 @@
-package com.maad.tripcalculator
+    package com.maad.tripcalculator
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.selection.selectable
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
-import com.maad.tripcalculator.Route.TRIP_PRICE
+    import androidx.compose.foundation.layout.Arrangement
+    import androidx.compose.foundation.layout.Column
+    import androidx.compose.foundation.layout.Row
+    import androidx.compose.foundation.layout.fillMaxSize
+    import androidx.compose.foundation.layout.padding
+    import androidx.compose.foundation.selection.selectable
+    import androidx.compose.material3.Button
+    import androidx.compose.material3.MaterialTheme
+    import androidx.compose.material3.RadioButton
+    import androidx.compose.material3.Text
+    import androidx.compose.runtime.Composable
+    import androidx.compose.runtime.getValue
+    import androidx.compose.runtime.mutableStateOf
+    import androidx.compose.runtime.remember
+    import androidx.compose.runtime.setValue
+    import androidx.compose.ui.Alignment
+    import androidx.compose.ui.Modifier
+    import androidx.compose.ui.res.stringResource
+    import androidx.compose.ui.tooling.preview.Preview
+    import androidx.compose.ui.unit.dp
+    import androidx.navigation.NavController
+    import androidx.navigation.compose.rememberNavController
+    import com.maad.tripcalculator.Route.TRIP_PRICE
 
-@Composable
-fun TrafficOptionsScreen(
-    navController: NavController,
-    km: Float,
-    time: Int,
-    modifier: Modifier = Modifier
-) {
-    var selected = ""
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = modifier.fillMaxSize()
+    @Composable
+    fun TrafficOptionsScreen(
+        navController: NavController,
+        km: Float,
+        time: Int,
+        modifier: Modifier = Modifier
     ) {
-        Column {
-            selected = getSelectedRadioButton(modifier)
-        }
-        Button(onClick = {
-            val traffic = when (selected) {
-                "Feeling Lucky (+0%)" -> 0.0
-                "Safe Zone (+5%)" -> 0.5
-                else -> 0.10
-            }
-            navController.navigate("${TRIP_PRICE}/$km/$time/$traffic")
-        }, modifier = modifier.padding(top = 32.dp))
-        {
-            Text(text = stringResource(R.string.calculate_trip_price))
-        }
-    }
-}
-
-@Composable
-fun getSelectedRadioButton(modifier: Modifier = Modifier): String {
-    var selectedRadioButton by remember { mutableStateOf("Feeling Lucky (+0%)") }
-
-    val trafficOptions =
-        arrayOf("Feeling Lucky (+0%)", "Safe Zone (+5%)", "Heavy Traffic (+10%)")
-
-    trafficOptions.forEach { item ->
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = modifier.selectable(
-                selected = selectedRadioButton == item,
-                onClick = { selectedRadioButton = item }
-            )
+        var selected = ""
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.fillMaxSize()
         ) {
-            RadioButton(
-                selected = selectedRadioButton == item,
-                onClick = {
-                    selectedRadioButton = item
-                })
-            Text(
-                text = item,
-                style = MaterialTheme.typography.bodyLarge,
-            )
+            Column {
+                selected = getSelectedRadioButton(modifier)
+            }
+            Button(onClick = {
+                val traffic = when (selected) {
+                    "Feeling Lucky (+0%)" -> 0.0
+                    "Safe Zone (+5%)" -> 0.05
+                    else -> 0.10
+                }
+                navController.navigate("${TRIP_PRICE}/$km/$time/$traffic")
+            }, modifier = modifier.padding(top = 32.dp))
+            {
+                Text(text = stringResource(R.string.calculate_trip_price))
+            }
         }
     }
-    return selectedRadioButton
-}
 
-@Preview(showBackground = true)
-@Composable
-fun TrafficOptionsScreenPreview() {
-    TrafficOptionsScreen(rememberNavController(), 20.5f, 12)
-}
+    @Composable
+    fun getSelectedRadioButton(modifier: Modifier = Modifier): String {
+        var selectedRadioButton by remember { mutableStateOf("Feeling Lucky (+0%)") }
+
+        val trafficOptions =
+            arrayOf("Feeling Lucky (+0%)", "Safe Zone (+5%)", "Heavy Traffic (+10%)")
+
+        trafficOptions.forEach { item ->
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier.selectable(
+                    selected = selectedRadioButton == item,
+                    onClick = { selectedRadioButton = item }
+                )
+            ) {
+                RadioButton(
+                    selected = selectedRadioButton == item,
+                    onClick = {
+                        selectedRadioButton = item
+                    })
+                Text(
+                    text = item,
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+            }
+        }
+        return selectedRadioButton
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun TrafficOptionsScreenPreview() {
+        TrafficOptionsScreen(rememberNavController(), 20.5f, 12)
+    }
--- Trip Calculator/app/src/main/java/com/maad/tripcalculator/TripPriceScreen.kt
+++ Trip Calculator/app/src/main/java/com/maad/tripcalculator/TripPriceScreen.kt
@@ -1,9 +1,34 @@
 package com.maad.tripcalculator
 
-import android.util.Log
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.maad.tripcalculator.Route.START
 
 @Composable
 fun TripPriceScreen(
@@ -13,5 +38,62 @@ fun TripPriceScreen(
     traffic: Float,
     modifier: Modifier = Modifier
 ) {
-    Log.d("trace", "KM: $km, TIME: $time, TRAFFIC: $traffic")
+    val snackbarHostState = remember { SnackbarHostState() }
+    val context = LocalContext.current
+    //Scaffold implements the basic visual layout structure (fab - top bar - bottom bar)
+    Scaffold(
+        //Component to host Snackbars
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { contentPadding ->
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier
+                .fillMaxSize()
+                .padding(contentPadding)
+
+        ) {
+            var price by remember { mutableIntStateOf(0) }
+            val priceCounter by animateIntAsState(
+                targetValue = price,
+                animationSpec = tween(
+                    durationMillis = 4000,
+                    easing = FastOutSlowInEasing
+                ), label = "result_animation"
+            )
+            LaunchedEffect(Unit) {
+                price = calculatePrice(km, time, traffic)
+                val result = snackbarHostState.showSnackbar(
+                    message = context.getString(R.string.price_calculated_successfully),
+                    actionLabel = context.getString(R.string.start_over),
+                    duration = SnackbarDuration.Indefinite
+                )
+                if (result == SnackbarResult.ActionPerformed)
+                    navController.popBackStack(START, false)
+            }
+            Text(text = stringResource(R.string.estimated_trip_price_is))
+            Text(
+                text = priceCounter.toString(),
+                fontSize = 60.sp,
+                fontWeight = FontWeight.ExtraBold
+            )
+            Text(text = stringResource(R.string.egp))
+        }
+    }
 }
+//}
+
+@Preview(showBackground = true)
+@Composable
+fun TripPriceScreenPreview() {
+    TripPriceScreen(rememberNavController(), 16.5f, 30, 0.0f)
+}
+
+fun calculatePrice(km: Float, time: Int, traffic: Float): Int {
+    val baseFare = 8
+    val kmPrice = km * 7.5
+    var total = baseFare + kmPrice + time
+    total += (total * traffic)
+    return total.toInt()
+}
+
