3308b639cc68aec67564543037d7d3f49967534b
AhmedMaad <atef.ahmed1341@gmail.com>
2024-07-10 04:06:16 +0200

    Lazy Column

--- Endangered Animals/app/src/main/java/com/maad/endangeredanimals/MainActivity.kt
+++ Endangered Animals/app/src/main/java/com/maad/endangeredanimals/MainActivity.kt
@@ -1,54 +1,40 @@
 package com.maad.endangeredanimals
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.text.ClickableText
-import androidx.compose.material3.Button
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.unit.dp
 import com.maad.endangeredanimals.data.DataSource
 import com.maad.endangeredanimals.model.Animal
 import com.maad.endangeredanimals.ui.theme.EndangeredAnimalsTheme
+import com.maad.endangeredanimals.ui.theme.PowderBlue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,9 +46,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //AnimalsList()
-                    AnimalsListItem(animal = DataSource().getAnimalsData().get(0))
-
+                    AnimalsList(DataSource().getAnimalsData())
                 }
             }
         }
@@ -71,14 +55,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AnimalsList(animals: List<Animal>, modifier: Modifier = Modifier) {
-
+    LazyColumn {
+        items(animals) {
+            AnimalsListItem(animal = it)
+        }
+    }
 }
 
 @Composable
 fun AnimalsListItem(animal: Animal, modifier: Modifier = Modifier) {
     Card(
-        colors = CardDefaults.cardColors(containerColor = Color.Gray),
-        modifier = modifier.fillMaxWidth()
+        colors = CardDefaults.cardColors(containerColor = PowderBlue),
+        modifier = modifier.fillMaxWidth().padding(8.dp)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
@@ -134,5 +122,5 @@ fun AnimalsListItem(animal: Animal, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 private fun ListItemPreview() {
-    AnimalsListItem(animal = DataSource().getAnimalsData().get(0))
+    AnimalsListItem(animal = DataSource().getAnimalsData()[0])
 }
