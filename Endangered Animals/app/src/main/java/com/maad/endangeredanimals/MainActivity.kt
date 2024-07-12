31c33b346df01cbb49d8727ef2bcb18f32846aba
AhmedMaad <atef.ahmed1341@gmail.com>
2024-07-12 02:06:14 +0200

    save the animal alert dialog

--- Endangered Animals/app/src/main/java/com/maad/endangeredanimals/MainActivity.kt
+++ Endangered Animals/app/src/main/java/com/maad/endangeredanimals/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.maad.endangeredanimals
 
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -15,14 +17,22 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
@@ -32,6 +42,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import com.maad.endangeredanimals.data.DataSource
 import com.maad.endangeredanimals.model.Animal
 import com.maad.endangeredanimals.ui.theme.EndangeredAnimalsTheme
@@ -64,6 +75,13 @@ fun AnimalsList(animals: List<Animal>) {
 
 @Composable
 fun AnimalsListItem(animal: Animal, modifier: Modifier = Modifier) {
+
+    //State Hoisting
+    var isDialogShown by remember { mutableStateOf(false) }
+    AnimalHelperDialog(animal = animal, isDialogShown = isDialogShown) {
+        isDialogShown = it
+    }
+
     Card(
         colors = CardDefaults.cardColors(containerColor = PowderBlue),
         modifier = modifier
@@ -78,8 +96,7 @@ fun AnimalsListItem(animal: Animal, modifier: Modifier = Modifier) {
                 painter = painterResource(id = animal.picture), contentDescription = stringResource(
                     id = animal.name
                 ),
-                modifier = modifier
-                    .size(120.dp)
+                modifier = modifier.size(120.dp)
             )
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
@@ -102,27 +119,82 @@ fun AnimalsListItem(animal: Animal, modifier: Modifier = Modifier) {
                 //above text will take the remaining space
                 Text(
                     buildAnnotatedString {
-                        //append(stringResource(id = R.string.save))
-                        withStyle(
-                            SpanStyle(
-                                fontWeight = FontWeight.Bold
+                        withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                            append(
+                                stringResource(
+                                    id = R.string.save_the_animal,
+                                    stringResource(id = R.string.save), "", ""
+                                )
                             )
-                        ) {
-                            append(stringResource(id = R.string.save))
                         }
-                        append(stringResource(id = R.string.the_animal))
+                        append(
+                            stringResource(
+                                id = R.string.save_the_animal,
+                                "", stringResource(id = R.string.the_animal), ""
+                            )
+                        )
                     },
-                    modifier = modifier.clickable {
-                    }
+                    modifier = modifier.clickable { isDialogShown = true }
                 )
             }
         }
     }
 }
 
+@Composable
+fun AnimalHelperDialog(
+    animal: Animal,
+    isDialogShown: Boolean,
+    onShowDialogChange: (Boolean) -> Unit
+) {
+
+    val context = LocalContext.current
+
+    when {
+        isDialogShown -> AlertDialog(
+            //called when the user tries to dismiss the Dialog by clicking outside
+            //or pressing the back button
+            onDismissRequest = { },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        val i = Intent(Intent.ACTION_VIEW, animal.link.toUri())
+                        context.startActivity(i)
+                        onShowDialogChange(false)
+                    }
+                ) { Text(text = stringResource(R.string.proceed)) }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = { onShowDialogChange(false) }
+                ) { Text(text = stringResource(R.string.cancel)) }
+            },
+            icon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_help),
+                    contentDescription = stringResource(R.string.help)
+                )
+            },
+            title = {
+                Text(
+                    text = stringResource(
+                        id = R.string.save_the_animal,
+                        stringResource(id = R.string.save),
+                        stringResource(id = R.string.the_animal),
+                        '?'
+                    )
+                )
+            },
+            text = {
+                Text(text = stringResource(id = animal.description))
+            }
+        )
+    }
+
+}
 
 @Preview(showBackground = true)
 @Composable
 private fun ListItemPreview() {
-    AnimalsListItem(animal = DataSource().getAnimalsData()[4])
+    AnimalsList(animals = DataSource().getAnimalsData())
 }
