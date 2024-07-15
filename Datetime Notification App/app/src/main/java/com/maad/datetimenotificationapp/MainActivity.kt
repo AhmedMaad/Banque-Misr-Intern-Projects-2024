eb0e6aa14484565b2698e359d1dceff40702d65e
AhmedMaad <atef.ahmed1341@gmail.com>
2024-07-15 03:18:33 +0200

    Date Picker Dialog

--- Datetime Notification App/app/src/main/java/com/maad/datetimenotificationapp/MainActivity.kt
+++ Datetime Notification App/app/src/main/java/com/maad/datetimenotificationapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.maad.datetimenotificationapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +9,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
@@ -17,10 +18,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TimePicker
 import androidx.compose.material3.TimePickerState
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberTimePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -29,7 +30,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.maad.datetimenotificationapp.ui.theme.DatetimeNotificationAppTheme
+import java.text.SimpleDateFormat
 import java.util.Calendar
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
 
@@ -56,6 +59,8 @@ fun DateTime(modifier: Modifier = Modifier) {
     ) {
         var time by remember { mutableStateOf("Choose a time") }
         var isTimePickerShown by remember { mutableStateOf(false) }
+        var date by remember { mutableStateOf("Choose a date") }
+        var isDatePickerShown by remember { mutableStateOf(false) }
 
         if (isTimePickerShown) {
             TimePickerChooser(
@@ -68,7 +73,19 @@ fun DateTime(modifier: Modifier = Modifier) {
                 })
         }
 
+        if (isDatePickerShown) {
+            DatePickerChooser(onConfirm = { dateState ->
+                val dateFormatter = SimpleDateFormat("dd-MM-yyyy", Locale.US)
+                val c = Calendar.getInstance()
+                c.timeInMillis = dateState.selectedDateMillis ?: 0L
+                date = "Selected date = ${dateFormatter.format(c.time)}"
+
+                isDatePickerShown = false
+            }, onDismiss = { isDatePickerShown = false })
+        }
+
         OutlinedButton(onClick = { isTimePickerShown = true }) { Text(text = time) }
+        OutlinedButton(onClick = { isDatePickerShown = true }) { Text(text = date) }
         OutlinedButton(onClick = {
             //show local notification with notification sent
         }) { Text(text = "Send notification") }
@@ -100,6 +117,40 @@ fun TimePickerChooser(
             TextButton(onClick = { onDismiss() }) { Text(text = stringResource(R.string.cancel)) }
         },
         text = { TimePicker(state = timePickerState) }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerChooser(
+    onConfirm: (DatePickerState) -> Unit,
+    onDismiss: () -> Unit,
+) {
+
+    val c = Calendar.getInstance()
+    val year = c.get(Calendar.YEAR)
+    val month = c.get(Calendar.MONTH) + 1
+    val day = c.get(Calendar.DAY_OF_MONTH)
+    val date = "$year/$month/$day"
+    val dateFormatter = SimpleDateFormat("yyyy/MM/dd", Locale.US)
+    val parsed = dateFormatter.parse(date)?.time ?: 0L
+
+    val datePickerState = rememberDatePickerState(
+        initialSelectedDateMillis = parsed,
+        yearRange = (2024..2025),
+    )
+
+    AlertDialog(
+        onDismissRequest = {},
+        confirmButton = {
+            TextButton(onClick = { onConfirm(datePickerState) }) {
+                Text(text = stringResource(R.string.ok))
+            }
+        },
+        dismissButton = {
+            TextButton(onClick = { onDismiss() }) { Text(text = stringResource(R.string.cancel)) }
+        },
+        text = { DatePicker(state = datePickerState) }
     )
 }
 
