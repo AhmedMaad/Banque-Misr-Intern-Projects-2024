fead6ee6cde619ca652c0561a580591923bf2173
AhmedMaad <atef.ahmed1341@gmail.com>
2024-07-18 03:29:21 +0200

    Incrementing Counter

--- Counter App/app/src/main/java/com/maad/counterapp/MainActivity.kt
+++ Counter App/app/src/main/java/com/maad/counterapp/MainActivity.kt
@@ -4,12 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.maad.counterapp.ui.theme.CounterAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,12 +47,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CounterApp(modifier: Modifier = Modifier){
-
+fun CounterApp(modifier: Modifier = Modifier) {
+    var counter by remember { mutableIntStateOf(0) }
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = counter.toString(),
+            fontSize = 60.sp,
+            style = TextStyle(
+                shadow = Shadow(
+                    color = Color.Blue,
+                    offset = Offset(5.0f, 10.0f),
+                    blurRadius = 3f
+                )
+            )
+        )
+        Button(
+            onClick = { ++counter },
+            colors = ButtonDefaults.buttonColors(containerColor = Color.Blue),
+            modifier = modifier.padding(top = 32.dp)
+        ) {
+            Text(text = stringResource(R.string.increment))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun CounterAppPreview(){
+fun CounterAppPreview() {
     CounterApp()
 }
