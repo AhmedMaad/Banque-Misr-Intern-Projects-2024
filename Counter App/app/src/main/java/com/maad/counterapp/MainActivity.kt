4242e3a8530e43bb84df3c2bed12ed9241b1a8a4
AhmedMaad <atef.ahmed1341@gmail.com>
2024-07-18 02:21:03 +0200

    initial commit

---
+++ Counter App/app/src/main/java/com/maad/counterapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.maad.counterapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.maad.counterapp.ui.theme.CounterAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CounterAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CounterApp(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CounterApp(modifier: Modifier = Modifier){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CounterAppPreview(){
+    CounterApp()
+}
