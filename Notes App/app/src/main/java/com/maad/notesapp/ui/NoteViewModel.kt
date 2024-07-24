bceceaa0fc6dd95e98f0463ca297ecf1e5ca351c
AhmedMaad <atef.ahmed1341@gmail.com>
2024-07-24 04:40:44 +0200

    using Disptachers.IO to execute operations off the main thread

--- Notes App/app/src/main/java/com/maad/notesapp/ui/NoteViewModel.kt
+++ Notes App/app/src/main/java/com/maad/notesapp/ui/NoteViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
 import com.maad.notesapp.database.Note
 import com.maad.notesapp.database.RoomDBHelper
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class NoteViewModel(app: Application) : AndroidViewModel(app) {
@@ -12,11 +13,11 @@ class NoteViewModel(app: Application) : AndroidViewModel(app) {
     private val db: RoomDBHelper = RoomDBHelper.getInstance(app)
 
     fun upsertNote(note: Note) {
-        viewModelScope.launch { db.noteDao.upsertNote(note) }
+        viewModelScope.launch(Dispatchers.IO) { db.noteDao.upsertNote(note) }
     }
 
     fun deleteNote(note: Note) {
-        viewModelScope.launch { db.noteDao.deleteNote(note) }
+        viewModelScope.launch(Dispatchers.IO) { db.noteDao.deleteNote(note) }
     }
 
     fun getNotes() = db.noteDao.getAllNotes()
