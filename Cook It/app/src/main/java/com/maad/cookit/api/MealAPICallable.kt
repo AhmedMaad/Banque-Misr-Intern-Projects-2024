d513c4668a294579e3360b12b988c2fe661ff17c
AhmedMaad <atef.ahmed1341@gmail.com>
2024-07-26 22:09:15 +0200

    Preparing Meal API Service

---
+++ Cook It/app/src/main/java/com/maad/cookit/api/MealAPICallable.kt
@@ -0,0 +1,24 @@
+package com.maad.cookit.api
+
+import com.maad.cookit.constants.Constants.CATEGORIES_ENDPOINT
+import com.maad.cookit.constants.Constants.CATEGORY_QUERY
+import com.maad.cookit.constants.Constants.FILTER_ENDPOINT
+import com.maad.cookit.constants.Constants.INGREDIENT_QUERY
+import com.maad.cookit.constants.Constants.LOOKUP_ENDPOINT
+import com.maad.cookit.model.CategoryRoot
+import com.maad.cookit.model.MealRoot
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface MealAPICallable {
+
+    @GET(CATEGORIES_ENDPOINT)
+    fun getCategories(): CategoryRoot
+
+    @GET(FILTER_ENDPOINT)
+    fun getMeals(@Query(CATEGORY_QUERY) categoryName: String): MealRoot
+
+    @GET(LOOKUP_ENDPOINT)
+    fun getRecipe(@Query(INGREDIENT_QUERY) mealId: String): MealRoot
+
+}
--- Cook It/app/src/main/java/com/maad/cookit/constants/Constants.kt
+++ Cook It/app/src/main/java/com/maad/cookit/constants/Constants.kt
@@ -8,4 +8,10 @@ object Constants {
     const val MEAL_IMAGE = "strMealThumb"
     const val MEAL_INSTRUCTIONS = "strInstructions"
     const val MEAL_VIDEO = "strYoutube"
+    const val BASE_URL = "https://www.themealdb.com"
+    const val CATEGORIES_ENDPOINT = "/api/json/v1/1/categories.php"
+    const val FILTER_ENDPOINT = "/api/json/v1/1/filter.php"
+    const val LOOKUP_ENDPOINT = "/api/json/v1/1/lookup.php"
+    const val CATEGORY_QUERY = "c"
+    const val INGREDIENT_QUERY = "i"
 }
